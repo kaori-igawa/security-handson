0b9b9c07fd1e7e297b6a18f4ecfe73ed385fc84c
kaori-igawa <kaori0v0igawa@gmail.com>
2026-02-16 09:45:12 +0900

    update : 任意のステータスコードを追加

--- handson/routes/api.js
+++ handson/routes/api.js
@@ -3,7 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send({message: 'Hello'});
+  let message = req.query.message;
+
+  console.log(message)
+
+  if (message === '') {
+    res.status(400);
+    message = 'messageの値が空です';
+  }
+  res.send({message});
 });
 
 router.use(express.json());
